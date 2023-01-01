ac50e8ad6bc5129a3d89690725f3419056865bd3
Timin8er <botballmaster@hotmail.com>
2023-01-01 12:50:08 -0500

    renamed config

---
+++ CosmicKSP/config.py
@@ -0,0 +1,60 @@
+import os
+from PyQtDataFramework.core.logging import logger, INFO, DEBUG
+from PyQtDataFramework.core import config_manager
+
+config_path = os.path.expanduser(os.path.join('~', 'Documents', 'CosmicKSP.config'))
+
+default_config = {
+    'LOGGING_LEVEL': INFO,
+    'COSMOS': {
+        'HOST': 'localhost',
+        'TELEMETRY_PORT': 8081,
+        'COMMANDS_PORT': 8080,
+    },
+    'TELEMACHUS':{
+        'HOST':'localhost',
+        'PORT':8085,
+        'FREQUENCY':500,
+    },
+    'KOS': {
+        'HOST':'localhost',
+        'PORT':5410,
+        'TIMEOUT':10,
+    },
+    # 'GAME_INSTANCES': [
+    #     {
+    #         'NAME':'REAL',
+    #         'DESCRIPTION':'The game instance representing the "real" world',
+    #         'DIR':'',
+    #         'GAME_NAME': 'default',
+    #         'TELEMACHUS':{
+    #             'HOST':'localhost',
+    #             'PORT':8085,
+    #             'FREQUENCY':500,
+    #         },
+    #         'KOS': {
+    #             'HOST':'localhost',
+    #             'PORT':5410,
+    #             'TIMEOUT':10,
+    #         },
+    #     },
+    #     {
+    #         'NAME':'SIM',
+    #         'DESCRIPTION':'The game instance representing a simulation',
+    #         'DIR':'',
+    #         'GAME_NAME': 'default',
+    #         'TELEMACHUS':{
+    #             'HOST':'localhost',
+    #             'PORT':8086,
+    #             'FREQUENCY':500,
+    #         },
+    #         'KOS': {
+    #             'HOST':'localhost',
+    #             'PORT':5411,
+    #             'TIMEOUT':10,
+    #         }
+    #     }
+    # ]
+}
+
+config = config_manager.get_config(config_path, default_config)
